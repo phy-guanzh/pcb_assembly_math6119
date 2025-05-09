6e520a9dd63657644cd26deec502ae536a5feacb
Zhe Guan <zhe.guan@cern.ch>
2025-05-09 19:41:44 +0100

    update

--- app1.py
+++ app1.py
@@ -63,7 +63,7 @@ if uploaded_files:
 else:
     st.info("No uploaded files. Using default ./solution files.")
     if st.button("🚀 Run machine_sim on Default Files"):
-        cmd = ["python3", "./machine_sim.py", "--strategy_folder", "./solution"]
+        cmd = ["python", "./machine_sim.py", "--strategy_folder", "./solution"]
         result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         st.subheader("📊 Simulation Output (Default)")
